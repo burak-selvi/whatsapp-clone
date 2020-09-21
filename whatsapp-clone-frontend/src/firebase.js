3772148fec71f64e3f0bb6a01ea3ce1ba6f8edef
Burak <burakselvi858@hotmail.com>
2020-09-22 01:45:08 +0300

    firebase config added

---
+++ whatsapp-clone-frontend/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCIVBbOj2zv8JpITRM0_kEUZjVNc-MpRkY",
+  authDomain: "whatsapp-clone-f410f.firebaseapp.com",
+  databaseURL: "https://whatsapp-clone-f410f.firebaseio.com",
+  projectId: "whatsapp-clone-f410f",
+  storageBucket: "whatsapp-clone-f410f.appspot.com",
+  messagingSenderId: "744077469834",
+  appId: "1:744077469834:web:5ba762136dbb9fc374a573",
+};
